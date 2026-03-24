4fb02f85498b5d1a2576e52a20bb956830139c75
Dan Kral <dan@bluestonelogic.com>
2026-03-24 09:27:47 -0400

    remove the rest of clerk file

--- frontend/src/components/Auth/index.ts
+++
@@ -1,2 +0,0 @@
-export { ClerkAuth } from './ClerkAuth';
-
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,7 +1,6 @@
 // App-specific components
-export { NetworkStatusIndicator } from './NetworkStatusIndicator';
-export * from './Auth';
-export * from './Layout';
-export * from './Message';
-export * from './Modals';
-export * from './Security';
+export { NetworkStatusIndicator } from "./NetworkStatusIndicator";
+export * from "./Layout";
+export * from "./Message";
+export * from "./Modals";
+export * from "./Security";
